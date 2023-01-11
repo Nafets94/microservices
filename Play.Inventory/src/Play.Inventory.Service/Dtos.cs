911882cf47d88b6e779928f5c591ba23e0413468
Nafets <effsty@gmail.com>
2023-01-11 16:13:33 +0200

    dtos for inventory microservice

---
+++ Play.Inventory/src/Play.Inventory.Service/Dtos.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Play.Inventory.Service
+{
+    public record GrantIdemsDto(Guid UserId, Guid CatalogItemId, int Quantity);
+
+    public record InventoryItemDto(Guid CatalogItemId, int Quantity, DateTimeOffset AcquiredDate);
+}
