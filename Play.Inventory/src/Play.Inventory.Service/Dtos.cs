f1b33229760c403d8c07d2de53d1ea7d19519e91
Nafets <effsty@gmail.com>
2023-01-11 21:01:37 +0200

    updated dtos and extension methods

--- Play.Inventory/src/Play.Inventory.Service/Dtos.cs
+++ Play.Inventory/src/Play.Inventory.Service/Dtos.cs
@@ -4,7 +4,7 @@ namespace Play.Inventory.Service
 {
     public record GrantIdemsDto(Guid UserId, Guid CatalogItemId, int Quantity);
 
-    public record InventoryItemDto(Guid CatalogItemId, int Quantity, DateTimeOffset AcquiredDate);
+    public record InventoryItemDto(Guid CatalogItemId, string Name, string Description, int Quantity, DateTimeOffset AcquiredDate);
 
     public record CatalogItemDto(Guid Id, string Name, string Description);
 }
